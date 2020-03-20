c7f68d54349bd336b0fe25dad4c9d71ca081d2e1
BrownQi <brownqi@foxmail.com>
2020-03-20 23:10:12 +0800

    vue demo

---
+++ vue-demo/src/components/content-qxj/Content.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="cls-content-qxj">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {}
+</script>
+
+<style lang="scss">
+  .cls-content-qxj {
+    margin: 20px 50px 80px 10px;
+    height: 750px;
+    border: gainsboro solid 1px;
+    background: #ffffff;
+    border-radius: 10px;
+    box-shadow: 0px 0px 5px gray;
+  }
+</style>
---
+++ vue-demo/src/components/header-qxj/Header.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="cls-header-group-qxj">
+    <div class="cls-header-item-left-qxj">
+      <h2>LOGO</h2>
+    </div>
+    <div class="cls-header-item-right-qxj">
+      <div class="cls-header-item-right-group-qxj">
+        <span class="iconfont icon-yonghu"></span>
+        <span>您好，{{username}}</span>
+        <span>退出</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data(){
+      return{
+        username:''
+      }
+    },
+    created() {
+      this.username = this.global.currentUser;
+    }
+  }
+</script>
+
+<style lang="scss">
+  .cls-header-group-qxj {
+    flex: 0 0 70px;
+    background: #5ccec6;
+    height: 100%;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    box-shadow: 0 1px 5px #425261;
+
+
+    .cls-header-item-left-qxj{
+      flex: 0.45;
+    }
+
+    .cls-header-item-right-qxj{
+      flex: 0.45;
+      font-size: 16px;
+      .cls-header-item-right-group-qxj{
+        text-align: right;
+      }
+      span {
+        margin-right: 10px;
+      }
+    }
+  }
+</style>
